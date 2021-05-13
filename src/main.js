e971bbcdf88d91160acb4ff32952407962e41685
lprivette <lprivette@usgs.gov>
2021-05-13 19:40:09 -0400

    adds layout, leaflet, simple map

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import vuetify from '@/plugins/vuetify'
 import App from './App.vue'
 import uswds from 'uswds/dist/js/uswds'
 import leaflet from 'leaflet/dist/leaflet'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
-// USWDS
+// UI Framework
+Vue.use(vuetify)
+
+// For tracking states
+Vue.use(Vuex)
+
+// USWDS (may not use this)
 Vue.use(uswds);
 
 // Leaflet
 Vue.use(leaflet);
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
